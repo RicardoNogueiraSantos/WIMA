2be2d8d1b883368fa6847b1af318454710dffd42
Ricardo Nogueira <ricardns@gmail.com>
2024-09-20 12:45:41 -0700

    Doc update

--- samples/Contiki/server/build/cooja/obj/Image.d
+++
@@ -1,4 +0,0 @@
-build/cooja/obj/Image.o: Image.c include/Image.h \
- /home/user/contiki-ng/arch/platform/cooja/./contiki-conf.h
-Image.c include/Image.h :
- /home/user/contiki-ng/arch/platform/cooja/./contiki-conf.h :
